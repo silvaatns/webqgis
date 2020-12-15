c1f31e852ea7559324c6ff805bae93ebaf5b45d3
silvaatns <76059459+silvaatns@users.noreply.github.com>
2020-12-15 23:04:52 +0700

    Add files via upload

---
+++ data/titikacuanbdl_1.js
@@ -0,0 +1,1 @@
+var json_titikacuanbdl_1 = {"type":"FeatureCollection","name":"titikacuanbdl_1","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"1"},"geometry":{"type":"Point","coordinates":[105.272369310868484,-5.381220570810433]}}]}
